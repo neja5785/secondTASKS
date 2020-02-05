b00ae24c30d2a8f39c34c2325af8cfe8d4c59c47
Nedas Jankauskas <nedasj44@gmail.com>
2020-02-05 22:25:11 +0200

    sadsadas

--- src/com/itacademy/day2/EmployeeManager.java
+++ src/com/itacademy/day2/EmployeeManager.java
@@ -6,8 +6,8 @@ public class EmployeeManager {
     public static void main(String[] args) {
 
         Employee employee = new Employee();
-
-        
+        System.out.println("asdasdsa");
+        System.out.println("asdsadsadsaa");
         employee.setContractStartDate(LocalDate.now());
         employee.setPosition("Developer");
         employee.setContractType(ContractType.FULL_TIME);
